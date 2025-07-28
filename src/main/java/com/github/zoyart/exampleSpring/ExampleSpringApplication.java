b5eb1577adbbbc90a8548763f2b782ce22d0be3e
Arthur Tsoy <arthurtsoyx@yandex.ru>
2025-07-28 11:22:37 +0500

    Init project

---
+++ src/main/java/com/github/zoyart/exampleSpring/ExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.zoyart.exampleSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zoyart/exampleSpring/ExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zoyart.exampleSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
